efc0e752813e984453f34a597c007ff97752c11b
Tatiana Hanazaki <tati.hana@equinox.com>
2020-04-11 08:36:58 -0400

    Apply theme to the page background

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import JobList from './components/JobList';
 import useMediaQuery from '@material-ui/core/useMediaQuery';
 import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
 
 function App() {
   const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
@@ -19,6 +20,7 @@ function App() {
 
   return (
     <ThemeProvider theme={theme}>
+    <CssBaseline/>
     <div className="App">
       <JobList />
     </div>
