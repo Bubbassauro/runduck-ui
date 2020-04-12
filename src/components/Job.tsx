373a49d713b75f736d50cbf95f0a5b99a131b043
Tatiana Hanazaki <tati.hana@equinox.com>
2020-04-12 16:27:49 -0400

    Button to refresh from source
    - Display date the data was refreshed

--- src/components/Job.tsx
+++ src/components/Job.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import { Component } from 'react';
 import Box from '@material-ui/core/Box';
+import Button from '@material-ui/core/Button';
+import Grid from '@material-ui/core/Grid';
 import Link from '@material-ui/core/Link';
+import Refresh from '@material-ui/icons/Refresh';
 import { withTheme, Theme } from '@material-ui/core/styles';
 import Command from './Command';
 import Notification from './Notification';
+import { Typography } from '@material-ui/core';
 
 type JobProps = {
     data: any,
@@ -17,13 +21,23 @@ class Job extends Component<JobProps> {
         name: '',
         permalink: '',
         commands: [],
-        notification: {}
+        notification: {},
+        updated: ''
     }
 
-    componentDidMount() {
+    getUpdatedStr(updated:string) {
+        if (updated) {
+            const updated_dt = new Date(updated);
+            const updated_msg = `Last Updated: ${updated_dt.toString()}`
+            return updated_msg;
+        }
+        return "";
+    }
+
+    loadJobData(refresh:boolean=false) {
         const env = this.props.data["env"]
         const uuid = this.props.data["uuid"]
-        let url = `http://localhost:3825/api/job/${env}/${uuid}`
+        let url = `http://localhost:3825/api/job/${env}/${uuid}?force_refresh=${refresh}`
         fetch(url)
             .then(res => res.json())
             .then((data) => {
@@ -32,17 +46,39 @@ class Job extends Component<JobProps> {
                     permalink: data["permalink"],
                     description: data["description"],
                     commands: (data["sequence"] ? data["sequence"]["commands"] : []),
-                    notification: data["notification"]
+                    notification: data["notification"],
+                    updated: this.getUpdatedStr(data["updated"])
                 })
             })
             .catch(console.log)
     }
 
+    componentDidMount() {
+        this.loadJobData();
+    }
+
     render() {
         return (
             <Box pl={6} p={2} style={{backgroundColor: this.props.theme.palette.background.default}}>
-                <Link variant="h6" href={this.state.permalink} color="textPrimary"
-                    target="_blank">{this.state.name}</Link>
+                <Grid container>
+                    <Grid item style={{ flex: 1 }}>
+                        <Link variant="h6" href={this.state.permalink} color="textPrimary"
+                            target="_blank">{this.state.name}</Link>
+                    </Grid>
+                    <Grid item>
+                        <Typography noWrap variant="body2" color="textSecondary">{this.state.updated}
+                            &nbsp;&nbsp;
+                            <Button
+                                variant="contained"
+                                size="small"
+                                startIcon={<Refresh/>}
+                                onClick={() => this.loadJobData(true)}
+                            >
+                            Refresh
+                            </Button>
+                        </Typography>
+                    </Grid>
+                </Grid>
                 {this.state.commands.map((command, i) => {
                     return (<Box key={i}>
                         <Command command={command} />
